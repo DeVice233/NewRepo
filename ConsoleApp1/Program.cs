2b5a0a001ea69317d4ff9ae26067093d9612b099
DeVice233 <yakovenko182003@gmail.com>
2020-11-02 16:44:22 +1000

    пара 02.11

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,11 +4,28 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp1
 {
+    class Cat
+    {
+        
+        public string Name;
+        public int Birthday;
+        public Cat(string name, int birthday) { Name = name; Birthday = birthday; }
+        public void MakeNoise()
+        {
+            Console.WriteLine($"{Name} мяукает");
+        }
+        public void GetAge()
+        {
+            Console.WriteLine($"Кошке по имени {Name} уже {Birthday} лет") ; 
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Cat Boris = new Cat("Boris", 10);
+            Boris.MakeNoise();
+            Boris.GetAge();
         }
     }
 }
