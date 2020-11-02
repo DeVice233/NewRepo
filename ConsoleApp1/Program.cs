e87e2c6224bde97de50fb6c190fb7fcdb287c912
DeVice233 <yakovenko182003@gmail.com>
2020-11-02 13:51:31 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
